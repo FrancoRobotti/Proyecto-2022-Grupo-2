f6e3f7809b7ac8cf10640e02619fba186ab2941a
Franco Robotti <FRANCO.ROBOTTI@correo.ucu.edu.uy>
2022-06-06 16:08:23 -0300

    ajustes player

--- src/Library/Player.cs
+++ src/Library/Player.cs
@@ -2,8 +2,8 @@
 public class Player     //Esta clase va a manejar el usuario y el tablero del jugador
 {
     //se crea la clase player  y se agregan lo atributos de User y GameBoard
-    public User user {get; set;}
-    private GameBoard gameBoard {get; set;}
+    private User user;
+    private GameBoard gameBoard;
 
     public Player(User user, GameBoard gameBoard)
     {
