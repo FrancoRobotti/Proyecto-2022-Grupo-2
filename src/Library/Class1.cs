b7f5a0dfdfb6c4ee1a86e7faae617c81c1704c83
Franco Robotti <FRANCO.ROBOTTI@correo.ucu.edu.uy>
2022-06-05 22:14:36 -0300

    se agrega la clase user

--- src/Library/Class1.cs
+++ src/Library/Class1.cs
@@ -1,5 +1,15 @@
-﻿namespace Library;
-public class Class1
+﻿namespace Proyecto;
+public class User   //Esta clase va a manejar un nickname del usuario y la funcionalidad de MatchMaking
 {
+    //se crea la clase User, se agrega el atributo de NickName y el metodo MatchMaking()
+
+    public string NickName { get; set; }
+
+    public void MatchMaking() //hay que ver exactamente que hace esta funcion
+    {
+
+    }
+
+
 
 }
