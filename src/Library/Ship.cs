8376b78f5503d4559ea9f4f07818f459f5b55e3d
Juan-Tabarez <jtabarez6@gmail.com>
2022-06-09 14:44:10 -0300

    Se crea clase Ship

---
+++ src/Library/Ship.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NavalBattle
+{
+    public class Ship
+    {
+        private string initialCoord;
+
+        private string direction;
+
+        private int length;
+
+        private bool sunk;
+
+        private List<string> coords;
+
+        public List<string> Coords 
+        {
+            get
+            {
+                return coords;
+            }
+        }
+        
+        public Ship (int length, string initialCoord, string direction)
+        {
+            this.length = length;
+            this.initialCoord = initialCoord;
+            this.direction = direction;
+            this.sunk = false;
+            this.coords = new List<string>();
+        }
+
+
+        public void AddShipCoord(string coord)
+        {
+            this.coords.Add(coord);
+        }
+    }
+}
