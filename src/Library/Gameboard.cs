44aa3144fc99426b97771143aff755d5158ea533
Juan-Tabarez <jtabarez6@gmail.com>
2022-06-09 14:48:57 -0300

    Se agrega clase Gameboard

---
+++ src/Library/Gameboard.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NavalBattle
+{
+    public class Gameboard
+    {   
+        private int side;
+
+        private string[,] gameboard;
+
+        private List<Ship> ships;
+
+        public Gameboard (int side)
+        {
+            this.side = side;
+            this.gameboard = new string[side,side];
+            this.ships = new List<Ship>();
+        }   
+
+        //Los Ship se crean en Gameboard por creator.
+        public void addShip(int length, string initialCoord, string direction)
+        {
+            Ship ship = new Ship(length, initialCoord, direction);
+            
+            int initialCoordX = (int)Char.GetNumericValue(initialCoord[0]);
+            int initialCoordY = (int)Char.GetNumericValue(initialCoord[1]);
+
+            bool validShip = true;
+
+            //El Ship es una lista de coordenadas, donde el usuario ingresa la coordenada inicial, largo y direccion del barco. 
+            //Con estos datos se checkea si es valida la posicion del barco en el tablero y se agregan al barco el resto de sus coordenadas. 
+            if ((direction == "N") && (initialCoordX - length >= -1))
+            {
+                for (int i = 0; i < length; i++)
+                {                                      
+                    ship.AddShipCoord(initialCoordX.ToString() + initialCoordY.ToString());
+                    initialCoordX--; 
+                }
+            }  
+            else if ((direction == "E") && (initialCoordY + length <= this.side))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    ship.AddShipCoord(initialCoordX.ToString() + initialCoordY.ToString());
+                    initialCoordY++;
+                }
+            }
+            else if ((direction == "S") && (initialCoordX + length <= this.side))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    ship.AddShipCoord(initialCoordX.ToString() + initialCoordY.ToString());
+                    initialCoordX++;
+                }
+            }
+            else if ((direction == "W") && (initialCoordY - length >= -1))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    ship.AddShipCoord(initialCoordX.ToString() + initialCoordY.ToString());
+                    initialCoordY++;
+                }
+            }
+            else
+            {
+                validShip = false;
+                Console.WriteLine("Barco fuera de rango");
+            }
+
+            //Si el barco fue creado con exito, tambien tenemos que evaluar que no comparta ninguna de sus coordenadas con otro barco.
+            if (validShip)
+            {
+                int validShipCounter = 0;
+
+                foreach (Ship placedShip in ships)
+                {
+                    foreach (string coord in ship.Coords)
+                    {
+                        if (placedShip.Coords.Contains(coord))
+                        {
+                            validShipCounter += 1;
+                        }
+                    }
+                }
+
+                if (validShipCounter == 0)
+                {
+                    ships.Add(ship);
+                    Console.WriteLine("Barco colocado correctamente");
+
+                }
+                else
+                {
+                    Console.WriteLine("Barcos superpuestos");
+                }
+            }     
+        }
+    }
+}
