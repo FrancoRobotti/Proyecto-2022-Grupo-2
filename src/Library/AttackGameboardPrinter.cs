8ac2b88644a819c01aa56f093f9ad677a774a9c5
Juan-Tabarez <jtabarez6@gmail.com>
2022-06-15 18:40:51 -0300

    Se incorpora que el el tablero de ataque muestre cuando un barco fue tocado

--- src/Library/AttackGameboardPrinter.cs
+++ src/Library/AttackGameboardPrinter.cs
@@ -3,7 +3,6 @@ using System.Text;
 
 namespace NavalBattle
 {
-    //Imprime el tablero sin mostrar la posicion de los barcos, solamente muestra donde se efectuaron los ataques.
     public class AttackGameboardPrinter : IPrinter
     {
         public void PrintGameboard(IGameboardContent gameboardContent)
@@ -18,11 +17,15 @@ namespace NavalBattle
             {
                 for (int j = 0; j < lenght; j++)
                 {
-                    if (gameboard[i,j] == "x")
+                    if (gameboard[i,j] == "t")
+                    {
+                        s.Append("|T|");
+                    }
+                    else if (gameboard[i,j] == "x")
                     {
                         s.Append("|x|");
                     }
-                    else
+                    else 
                     {
                         s.Append("| |");
                     }
